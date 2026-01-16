167ab40c049d03b9236d53798f926febfd292acd
yassin-serokh-vinci <yassin.serokh@student.vinci.be>
2026-01-16 17:25:23 +0100

    test fail when there is nothing before @

--- tests/emailValidator.test.ts
+++ tests/emailValidator.test.ts
@@ -4,4 +4,8 @@ describe("isValidEmail", () => {
   test("returns false when there is no @", () => {
     expect(isValidEmail("abc")).toBe(false);
   });
+
+  test("returns false when there is nothing before @", () => {
+  expect(isValidEmail("@domain.com")).toBe(false);
+});
 });
